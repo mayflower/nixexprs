c503c2bf98827625540660c1ed08cc074b630330
Robin Gloster <robin@loc-com.de>
2015-07-18 17:09:55 +0000

    add seperate php building

---
+++ php.nix
@@ -0,0 +1,13 @@
+/* A small release file, with few packages to be built.  The aim is to reduce
+   the load on Hydra when testing the `stdenv-updates' branch. */
+
+{ nixpkgs
+, supportedSystems ? [ "x86_64-linux" ]
+}:
+
+with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; };
+
+(mapTestOn (rec {
+  php = all;
+  php70 = all;
+} ))
