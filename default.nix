0b2724e4a7cb8438dceeab727ce9d003d7c08629
Tristan Helmich <fadenb@users.noreply.github.com>
2015-12-07 18:32:01 +0100

    Add perl package IOTty

--- default.nix
+++ default.nix
@@ -180,6 +180,7 @@ with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems;
     CGI = all;
     CGIFast = all;
     FCGI = all;
+    IOTty = all;
     LWP = all;
     NetDNS = all;
     NetLDAP = all;
