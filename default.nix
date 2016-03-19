947d4d5d90c75ef66558effb7aa17b1692eb1deb
Franz Pletz <fpletz@fnordicwalking.de>
2016-03-19 15:13:27 +0100

    Remove Linux 3.18 kernel series

--- default.nix
+++ default.nix
@@ -333,13 +333,6 @@ with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems;
     tools = all;
   };
 
-  linuxPackages_3_18 = {
-    kernel = all;
-    kernelHeaders = all;
-    perf = all;
-    virtualboxHardened = all;
-  };
-
   linuxPackages_4_1 = {
     kernel = all;
     kernelHeaders = all;
