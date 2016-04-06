dd511e9647cbb7b75026938bc22a7f9a6d7eb480
Franz Pletz <fpletz@fnordicwalking.de>
2016-04-06 15:34:50 +0200

    Remove linux kernel 4.1 & 4.3

--- default.nix
+++ default.nix
@@ -335,20 +335,6 @@ with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems;
     tools = all;
   };
 
-  linuxPackages_4_1 = {
-    kernel = all;
-    kernelHeaders = all;
-    perf = all;
-    virtualboxHardened = all;
-  };
-
-  linuxPackages_4_3 = {
-    kernel = all;
-    kernelHeaders = all;
-    perf = all;
-    virtualboxHardened = all;
-  };
-
   linuxPackages_4_4 = {
     kernel = all;
     kernelHeaders = all;
