b3b0443a7558422178de3cbbe25ebc2cdb8431cd
Franz Pletz <fpletz@fnordicwalking.de>
2016-07-13 19:53:35 +0200

    Fix reference to recursiveUpdate

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems;
 
 let
 
-  recursiveUpdate = nixpkgs.lib.recursiveUpdate;
+  recursiveUpdate = (import <nixpkgs> {}).lib.recursiveUpdate;
 
   kernelPackages = {
     kernel = all;
