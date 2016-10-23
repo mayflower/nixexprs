472c601b75a8d9a043c7d1f7d893a1fa8916a04a
Franz Pletz <fpletz@fnordicwalking.de>
2016-10-23 10:55:51 +0200

    Revert "add build for pr-19324"
    This reverts commit 368b7ea479eab8ef9a247d1e8ba0b795780bd140.

--- jobsets.nix
+++ jobsets.nix
@@ -60,13 +60,6 @@ let
       input = "nixpkgs";
       path = "doc/default.nix";
     };
-    pr-19324 = {
-      inputs = defaultSettings.inputs // {
-        nixpkgs = defaultSettings.inputs.nixpkgs // {
-          value = "https://github.com/Profpatsch/nixpkgs.git fix-stripHash";
-        };
-      };
-    };
   });
   fileContents = with pkgs.lib; ''
     cat <<EOF
