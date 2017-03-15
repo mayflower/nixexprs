2d3e4fc516be807d5ccddf03e5231d18bc4e591c
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-03-15 09:36:17 +0100

    Add missing attrpath

--- pull-request.nix
+++ pull-request.nix
@@ -5,6 +5,7 @@
 
 with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; };
 let
-  packages = nixpkgs.stdenv.lib.genAttrs pullRequestPackages (name: {name = all;});
+  pkgs = import nixpkgs {};
+  packages = with pkgs.lib; genAttrs pullRequestPackages (name: {name = all;});
 in
 (mapTestOn packages)
