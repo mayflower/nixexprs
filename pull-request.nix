fb3711f3a94a1f80268a92bb3d0463c77bc933b8
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-03-20 22:35:13 +0100

    Enhance pkgs scope

--- pull-request.nix
+++ pull-request.nix
@@ -6,7 +6,7 @@
 with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; };
 let
   pkgs = import nixpkgs {};
-  makeSet  = name: all;
-  packages = with pkgs.lib; genAttrs pullRequestPackages makeSet;
+  makeSet  = with pkgs; name: all;
+  packages = with pkgs; lib.genAttrs pullRequestPackages makeSet;
 in
 (mapTestOn packages)
