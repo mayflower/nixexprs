4c7633702b4859e74f7e07c6a8c8677b15f3152d
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-03-21 10:24:06 +0100

    Pimp pr.nix to handle attrPaths

--- pull-request.nix
+++ pull-request.nix
@@ -6,7 +6,19 @@
 with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; };
 let
   pkgs = import nixpkgs {};
-  makeSet  = with pkgs; name: all;
-  packages = with pkgs; lib.genAttrs pullRequestPackages makeSet;
+
+  makeSet  = with pkgs; name: value: all;
+  makePackages = with pkgs; paths: (map (x: lib.genAttrs x makeSet) paths);
+  makePaths = with pkgs; prPkgs: (map (x: ["pkgs"] ++ lib.splitString "." x) prPkgs);
+  packages = with pkgs; makePackages (makePaths pullRequestPackages);
+
+  # packages = with pkgs; lib.genAttrs pullRequestPackages makeSet;
+  # packages = with pkgs; lib.
+
+  # mapAttrByPath = with pkgs; path: attrs: lib.setAttrByPath path (lib.attrByPath path null attrs);
+  # filterAttrsByPath = with pkgs; paths: attrs: lib.foldl' lib.recursiveUpdate {} (map (x: mapAttrByPath x attrs) paths);
+  # filterPkgs = with pkgs; paths: pkgs: filterAttrsByPath (map (x: ["pkgs"] ++ lib.splitString "." x) paths) pkgs;
+
+  # packages = filterPkgs pullRequestPackages;
 in
-(mapTestOn packages)
+(map (x: mapTestOn x) packages)
