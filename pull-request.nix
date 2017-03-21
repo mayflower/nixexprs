36cff2fc9b8f2f3b550e9429aacaa4b7c528b128
Periklis Tsirakidis <periklis.tsirakidis@mayflower.de>
2017-03-21 13:50:00 +0100

    Enhance syntax

--- pull-request.nix
+++ pull-request.nix
@@ -6,12 +6,13 @@
 with import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; };
 let
   pkgs = import nixpkgs {};
+  lib = pkgs.lib;
 
   /*
    makeAttrs ["qt58.qtbase" "qcachegrind"]
      => {qt58 = {qtbase = all}; qcachegrind = all;}
   */
-  makeAttrs = with pkgs; paths: (map (p: lib.setAttrByPath (lib.splitString "." p) all) paths);
-  packages = with pkgs; lib.foldl (a: b: a // b) {} (makeAttrs pullRequestPackages);
+  makeAttrs = paths: (map (p: lib.setAttrByPath (lib.splitString "." p) all) paths);
+  packages = lib.foldl (a: b: a // b) {} (makeAttrs pullRequestPackages);
 in
 (mapTestOn packages)
