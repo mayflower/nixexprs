53246663b2fd6b8ead9f081e851a61b3c55deccd
Robin Gloster <mail@glob.in>
2017-08-10 23:46:35 +0200

    use mayflower fork, as fetching takes too long..

--- jobsets.nix
+++ jobsets.nix
@@ -75,7 +75,7 @@ let
       inputs.nixpkgs.value = "${defaultSettings.inputs.nixpkgs.value} openssl-1.1";
     };
     hydra-jobs-stdenv-set-u = {
-      inputs.nixpkgs.value = "git://github.com/obsidiansystems/nixpkgs stdenv-set-u";
+      inputs.nixpkgs.value = "${defaultSettings.inputs.nixpkgs.value} stdenv-set-u";
     };
     #hydra-jobs-arm = {
     #  path = "arm.nix";
