a60aeba056b13ab9787fb0a417398c509f9a23b7
WilliButz <willibutz@cyberfnord.de>
2017-08-31 19:06:38 +0200

    modules: add new kvm module

---
+++ kvm.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, ... }:
+with lib;
+{
+  options = {
+    mayflower.kvm.enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf config.mayflower.kvm.enable (mkMerge [
+    { hardware.enableAllFirmware = false;
+
+      boot.loader.grub.enable = true;
+      boot.loader.grub.device = "/dev/vda";
+      boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-label/nixos";
+        fsType = lib.mkDefault "ext4";
+      };
+    }
+    ((import <nixpkgs/nixos/modules/profiles/qemu-guest.nix>) { inherit config pkgs; })
+  ]);
+}
