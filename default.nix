a33b72f860ce3021f3186449e752e60bc192ce36
Franz Pletz <fpletz@fnordicwalking.de>
2018-02-07 00:14:23 +0100

    build linux 4.14

--- default.nix
+++ default.nix
@@ -717,6 +717,7 @@ in
   zsh = all;
 
   linuxPackages = recursiveUpdate kernelPackages kernelPackages_virtualbox;
+  linuxPackages_4_14 = recursiveUpdate kernelPackages kernelPackages_virtualbox;
   linuxPackages_latest = recursiveUpdate kernelPackages kernelPackages_virtualbox;
 
   nodePackages = {
