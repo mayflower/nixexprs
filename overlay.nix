8265635319b3d4f5ba4f30206d1e87d60bb75426
WilliButz <wbutz@cyberfnord.de>
2019-04-08 18:49:29 +0200

    oxidized: add patch to exclude tplink timestamp

--- overlay.nix
+++ overlay.nix
@@ -16,4 +16,16 @@ self: super:
   dovecot = super.dovecot.override { withPgSQL = true; };
   postfix = super.postfix.override { withPgSQL = true; };
   freeradius = super.freeradius.override { withJson = true; withRest = true; };
+
+  defaultGemConfig = super.defaultGemConfig // {
+    oxidized = (attrs: rec {
+      tplinkPatch = (super.fetchpatch {
+        url = https://patch-diff.githubusercontent.com/raw/ytti/oxidized/pull/1443.diff;
+        sha256 = "09dyf1hnxgdxfkh9l6y63qmm1ds5wgb2d52vvrwwc0s4gl0b1yad";
+      });
+      postInstall = ''
+        patch -p1 -d $(cat $out/nix-support/gem-meta/install-path) -i ${tplinkPatch}
+      '';
+    });
+  };
 }
