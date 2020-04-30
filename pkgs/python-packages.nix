fbf69a5367e759f34480ad03798d5e59d47b08f7
Linus Heckemann <git@sphalerite.org>
2020-04-30 08:09:37 +0200

    postorius: add patch to allow users to create lists

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -1,26 +1,14 @@
 self: super: {
-  aiosmtpd = super.callPackage ./python/aiosmtpd { };
   atpublic = super.callPackage ./python/atpublic { };
   automx = super.callPackage ./python/automx { };
   colorhash = super.callPackage ./python/colorhash { };
   coloredlogs = super.callPackage ./python/coloredlogs { };
-  cssmin = super.callPackage ./python/cssmin { };
   django-allauth = super.callPackage ./python/django-allauth { };
   django-gravatar2 = super.callPackage ./python/django-gravatar2 { };
-  django-mailman3 = super.callPackage ./python/django-mailman3 { };
-  django-paintstore = super.callPackage ./python/django-paintstore { };
-  django-q = super.callPackage ./python/django-q { };
   duckling = super.callPackage ./python/duckling { };
   fakeredis = super.callPackage ./python/fakeredis { };
   fbmessenger = super.callPackage ./python/fbmessenger { };
-  flufl-bounce = super.callPackage ./python/flufl-bounce { };
-  flufl-i18n = super.callPackage ./python/flufl-i18n { };
-  flufl-lock = super.callPackage ./python/flufl-lock { };
-  flufl-testing = super.callPackage ./python/flufl-testing { };
   humanfriendly = super.callPackage ./python/humanfriendly { };
-  lazr-config = super.callPackage ./python/lazr-config { };
-  lazr-delegates = super.callPackage ./python/lazr-delegates { };
-  lazr-smtptest = super.callPackage ./python/lazr-smtptest { };
   mattermostwrapper = super.callPackage ./python/mattermostwrapper { };
   pykwalify = super.callPackage ./python/pykwalify { };
   python3-openid = super.callPackage ./python/python3-openid { };
@@ -51,6 +39,10 @@ self: super: {
       sha256 = "1302fqsrx8w474xk5cmnmg3hjqfprlxnjg9qlg86arsr4v4vqm4b";
     };
     propagatedBuildInputs = with super; [ django python-dateutil whoosh pysolr elasticsearch setuptools_scm ];
+  });
+
+  postorius = super.postorius.overrideAttrs ({patches ? [], ...}: {
+    patches = patches ++ [ ./postorius_users_can_create_lists.patch ];
   });
 
   # geopy FIXME
