44cc11f41d6c9bc7d9a556bacd181b2f6e94bc7f
Linus Heckemann <git@sphalerite.org>
2020-09-24 11:15:21 +0200

    Support only en_US.UTF-8
    Closure size! This was originally set in nixpkgs[1][2] as the default
    of the option, but since it's a mayflower-local change let's set it
    here in nixexprs instead.
    
    [1]: https://github.com/mayflower/nixpkgs/commit/a2b883a1833a99121adfad0c5a5ac0e740897f87
    [2]: https://github.com/mayflower/nixpkgs/commit/4a84c173a9a6c811ad94c6fc3342c21a89d733ee

--- modules/base.nix
+++ modules/base.nix
@@ -35,6 +35,7 @@ with lib;
     };
 
     i18n.defaultLocale = "en_US.UTF-8";
+    i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "en_GB.UTF-8/UTF-8"];
 
     console = {
       font = "Lat2-Terminus16";
