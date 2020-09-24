e603e2da2e5169298612eced2dcb4ca638116797
Linus Heckemann <git@sphalerite.org>
2020-09-24 16:00:10 +0200

    fix docs jobset

--- hydra-jobs/docs.nix
+++ hydra-jobs/docs.nix
@@ -3,13 +3,13 @@
 , system ? builtins.currentSystem
 }:
 {
-  nixosManual = (import <nixpkgs/nixos/lib/eval-config.nix> {
+  nixosManual = (import (nixpkgs + "/nixos/lib/eval-config.nix") {
     inherit system;
     modules = [({ lib, ... }: {
       documentation.nixos.enable = lib.mkForce true;
     })];
-    baseModules = import <nixpkgs/nixos/modules/module-list.nix> ++ import <nixexprs/module-list.nix>;
+    baseModules = import (nixpkgs + "/nixos/modules/module-list.nix") ++ import (nixexprs + "/module-list.nix");
   }).config.system.build.manual;
 
-  nixpkgsManual = import <nixpkgs/doc/default.nix> {};
+  nixpkgsManual = import (nixpkgs + "/doc/default.nix") {};
 }
