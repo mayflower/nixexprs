6e8660fd8446c7d3d929e0b9aeb7d416e51fea7c
Maximilian Bosch <maximilian@mbosch.me>
2022-07-28 10:32:08 +0200

    hydra-jobs: allow to build everything that's marked as redistributable
    See the comment for further context.

--- hydra-jobs/default.nix
+++ hydra-jobs/default.nix
@@ -10,6 +10,18 @@ import ./packages.nix {
     nixpkgsArgs = {
       config = {
         allowUnfree = false;
+
+        # As soon as something is redistributable, it's legally OK to - well - redistribute
+        # it in our binary cache. Nixpkgs is stricter here because of political reasons,
+        # i.e. everything that's not approved as free by e.g. OSI won't be
+        # redistributed with only a *few* exceptions such as firmware code.
+        #
+        # However, we want mongodb to be built by our Hydra.
+        allowUnfreePredicate = { meta ? {}, ... }:
+          let
+            license = meta.license or {};
+          in
+          ((builtins.isAttrs license) && (license.redistributable or false));
         inHydra = true;
       };
       overlays = [ (import "${nixexprs}/overlay.nix") ];
