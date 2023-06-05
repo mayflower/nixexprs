8143e831be7b65710276c4fec7961986bb429687
Maximilian Bosch <maximilian@mbosch.me>
2023-06-05 12:01:31 +0200

    hydra-jobs: fix infinite recursion issues
    We don't need these `overridePythonAttrs`-things anymore because no ARM
    stuff is built for now.
    
    Also, this causes infinite recursion errors[1] because the override
    silences the `.override { enableDocumentation = false; }` of `hypothesis`
    which is necessary to break a dependency loop (chardet -> hypothesis ->
    sphinx (docs) -> ... -> hypothesis) that causes the infinite recursion.
    
    [1] https://hydra.mayflower.de/eval/3266?filter=aarch64&compare=3264&full=#tabs-errors

--- pkgs/python-packages.nix
+++ pkgs/python-packages.nix
@@ -7,17 +7,4 @@ self: super: {
   rasa-core = self.callPackage ./python/rasa-core { };
   rasa-nlu = self.callPackage ./python/rasa-nlu { };
   sklearn-crfsuite = self.callPackage ./python/sklearn-crfsuite { };
-  # geopy FIXME
-  pytest-xdist = super.pytest-xdist.overridePythonAttrs(old: {
-    doCheck = super.pytest-xdist.system != "aarch64-linux";
-  });
-  hypothesis = super.hypothesis.overridePythonAttrs(old: {
-    doCheck = super.hypothesis.system != "aarch64-linux";
-  });
-  requests = super.requests.overridePythonAttrs(old: {
-    doCheck = super.requests.system != "aarch64-linux";
-  });
-  curio = super.curio.overridePythonAttrs(old: {
-    doCheck = super.curio.system != "aarch64-linux";
-  });
 }
