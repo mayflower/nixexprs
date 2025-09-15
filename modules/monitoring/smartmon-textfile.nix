2b80a20f9dff3fdb3294c9bb3bad01b3b819f54d
Christian Albrecht <christian.albrecht@mayflower.de>
2025-09-15 10:00:09 +0200

    monitoring/smartmon-textfile: patch smartmon.py for 3ware disks
    because smartctl is not detecting 3ware controlled disks correctly,
    add the possibility to inject devices through
    /etc/smartmon-devices.txt instead of finding via --scan-open.
    
    # cat /etc/smartmon-devices.txt
    /dev/twl0 -d 3ware,0
    /dev/twl0 -d 3ware,1
    ...
    
    3w-sas: scsi0: ERROR: (0x03:0x0101): Invalid command opcode:opcode=0x4D.

--- modules/monitoring/smartmon-textfile.nix
+++ modules/monitoring/smartmon-textfile.nix
@@ -1,16 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let
-  prometheus-community-scripts = pkgs.fetchFromGitHub rec {
-    owner  = "prometheus-community";
-    repo   = "node-exporter-textfile-collector-scripts";
-    rev    = "71a86190cef829a8d4fdadf86bda811a5b1c04a1";
-    sha256 = "0xw77yk6a3mqbwjrrwpjvcsjy5nfljbfzkiridhm4masldx1hllr";
+  prometheus-community-scripts = pkgs.stdenv.mkDerivation {
+    pname = "prometheus-community-scripts";
+    version = "2025-09-28";
+    src = pkgs.fetchFromGitHub {
+      owner  = "prometheus-community";
+      repo   = "node-exporter-textfile-collector-scripts";
+      rev    = "7ef6216b1acced3e1bd6bca9165586981deeaaff";
+      sha256 = "sha256-UcmQSJf20HHJa87RJ+Kc4OVK88VgjYAFKftV37wu5oQ=";
+    };
+    patches = [ ./smartmon-textfile-3ware.patch ];
+    installPhase = ''
+      mkdir $out
+      cp smartmon.py $out/smartmon.py
+    '';
   };
 
   smartmon-script = "${prometheus-community-scripts}/smartmon.py";
   metrics-file = "/run/prometheus-node-exporter/prometheus_smartmon.prom";
   prometheus-group = config.systemd.services.prometheus-node-exporter.serviceConfig.Group;
+  python = (pkgs.python3.withPackages(ps: [ps.prometheus_client])).interpreter;
 in
 {
   config = lib.mkIf (!config.boot.isContainer && !config.mayflower.kvm.enable) {
@@ -38,7 +48,7 @@ in
           bindsTo = [ "prometheus-node-exporter.service" ];
           path = [ pkgs.smartmontools ];
           script = ''
-            ${pkgs.python3}/bin/python ${smartmon-script} | ${pkgs.moreutils}/bin/sponge ${metrics-file}
+            ${python} ${smartmon-script} | ${pkgs.moreutils}/bin/sponge ${metrics-file}
           '';
           serviceConfig = {
             Type = "oneshot";
